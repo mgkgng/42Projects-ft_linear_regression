affd8a88e2dda73cd4df65ffb1707fca11e5db21
Minguk Gaang <minguk.gaang@gmail.com>
2022-11-05 22:13:24 +0100

    almost done

--- LRModel.py
+++ LRModel.py
@@ -1,12 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 class LRModel:
-	def __init__(self):
-		self.weight = 0
-		self.bias = 0
-		self.dataset = []
+	def __init__(self, alpha=0.001, max_iter=1000):
+		self.thetas = np.zeros(2, 1)
+		print(self.thetas)
+		self.alpha = alpha
+		self.max_iter = max_iter
+		self.x = []
+		self.y_hat = []
 
 	def estimatePrice(self, mileage):
-		self.dataset.append([mileage, self.weight * mileage + self.bias])
-		return self.dataset[-1][1]
+		np.array(self.x, mileage)
+		np.array(self.y_hat, mileage * self.thetas[1] + self.thetas[0])
+		return self.y_hat[-1]
 
-	def trainModel(self):
-		pass
+	def fit(self, y):
+		x = np.insert(self.x, 0, 1, 1)
+		for n in range(self.max_iter):
+			res_gradient = np.array(x.T @ (x @ self.thetas - y) / y.shape[0])
+			self.thetas = self.thetas - self.alpha * res_gradient
+
+	def plot(self):
+		plt.show()
