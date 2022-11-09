480e8a22abf14f88136d98e14847c8f7b8562752
Minguk Gaang <minguk.gaang@gmail.com>
2022-11-09 13:09:21 +0100

    my model doesn't work so i'm changing everything from the beginning

--- LRModel.py
+++ LRModel.py
@@ -1,8 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class LRModel:
-	def __init__(self, alpha=0.001, max_iter=1000):
-		self.thetas = [0, 0]
+	def __init__(self, alpha=0.001, max_iter=100000):
+		self.weight = -2
+		self.bias = 500000
 		self.alpha = alpha
 		self.max_iter = max_iter
 
@@ -10,7 +12,30 @@ class LRModel:
 		return self.weight * mileage + self.bias
 
 	def trainModel(self, x, y):
+		for _ in range(self.max_iter):
+			self.weight, self.bias = self.step_gradient(x, y)
+
+	def cost_func(self, x, y):
+		cost = self.predict(x) - y
+		return (cost.T @ cost) / (cost.size * 2)
+
+	def step_gradient(self, x, y):
+		for _ in range(x.size):
+			pass
+
+	def predict(self, x):
 		x_prime = np.insert(x, 0, 1, 1)
-		for n in range(self.max_iter):
-			res = (x_prime @ (x_prime @ self.thetas - y)) / y.shape[0]
-			self.thetas = self.thetas - self.alpha * res
+		return  np.insert(x, 0, 1, 1) @ np.array([[self.bias], [self.weight]])
+
+	def loss_elem(self, x, y):
+		pass
+
+	def loss(self, x, y):
+		x_prime = np.insert(x, 0, 1, 1)
+		print(x_prime.shape)
+		return x_prime.T @ (x_prime @ np.array([[self.bias], [self.weight]])) / (x_prime.shape[0] * 2)
+
+	def plot(self, x, y):
+		plt.plot(x, y, 'o')
+		plt.plot(x, self.predict(x))
+		plt.show()
--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import pandas
 import numpy as np
 from LRModel import LRModel
 
+def zscore(x):
+	vfunc = np.vectorize(lambda e : (e - np.mean(x)) / np.std(x))
+	return vfunc(x)
+
 if __name__=="__main__":
 	data = np.array(pandas.read_csv('data.csv'))
 	X = data[:,0].reshape(-1, 1)
 	Y = data[:,1].reshape(-1, 1)
-	xx = LRModel()
-	xx.trainModel(X, Y)
-	while True:
-		s = input("Enter the mileage to get the estimate the price of your car, or insert QUIT\n>>> ")
-		if s == "QUIT":
-			exit()
-		elif not s.isdigit():
-			print("Wrong input")
-		else:
-			mileage = int(s)
-			print("The estimated price is ${}".format(xx.estimatePrice(mileage)))
+	lr = LRModel()
+	print(lr.loss(X, Y))
+	# lr.trainModel(zscore(X), zscore(Y))
+	# while True:
+	# 	s = input("Enter the mileage to get the estimate the price of your car, or insert QUIT\n>>> ")
+	# 	if s == "QUIT":
+	# 		exit()
+	# 	elif not s.isdigit():
+	# 		print("Wrong input")
+	# 	else:
+	# 		mileage = int(s)
+	# 		print("The estimated price is ${}".format(lr.estimatePrice(mileage)))
+	# 		print(lr.thetas)
+	# 		lr.plot(X, Y)
